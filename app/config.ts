fd9e0dba13a00243a2f59927b2c32d8a20e97a04
PrajwalUppoor <36199449+PrajwalUppoor@users.noreply.github.com>
2025-11-14 16:51:51 +0530

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -4,7 +4,7 @@
 // For Android emulator: http://10.0.2.2:8080
 // For physical device: http://YOUR_LOCAL_IP:8080 or use ngrok
 
-export const API_BASE_URL = 'https://iot-gateway-api-service.onrender.com' 
+export const API_BASE_URL = 'http://localhost:8080';
 export const API_CONFIG = {
   timeout: 10000,
   headers: {
